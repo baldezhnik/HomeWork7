89b11aec29be00d9325ac8264231ca82ffbb7b0b
baldezhnik <drako.dima@gmail.com>
2022-03-17 23:06:28 +0300

    Реализованно первое задание

---
+++ src/com/dsd/Hw.java
@@ -0,0 +1,16 @@
+package com.dsd;
+
+public class Hw {
+    
+    String first (String str) {
+        String strout = str.substring(0,4)+str.substring(8,13);
+        return strout;
+    }
+
+    String second (String str) {
+        String strout = null;
+
+        return strout;
+    }
+    
+}
--- src/com/dsd/Main.java
+++ src/com/dsd/Main.java
@@ -6,8 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Hw hw = new Hw();
         Scanner sc = new Scanner(System.in);
         String documentNum = sc.nextLine();
+
+        System.out.println(hw.first(documentNum));
+
 
 
 
