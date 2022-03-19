c6c1e4ca0b1ed99a5d7a8ef188511a5858c4f92e
baldezhnik <drako.dima@gmail.com>
2022-03-19 19:03:35 +0300

    теперь есть проверка ввода, и реализована 2-ая задача.

--- src/com/dsd/Hw.java
+++ src/com/dsd/Hw.java
@@ -1,5 +1,7 @@
 package com.dsd;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Locale;
 
 public class Hw {
@@ -16,8 +18,10 @@ public class Hw {
     }
 
     String third (String str) {
-        String strout = null;
-
+        String[] st = str.split ("-");
+        String[] st1 = st[4].split("");
+        String[] st2 = {st[1],st[3],st1[1],st1[3]};
+        String strout = String.join("/", st2);
         return strout;
     }
 
@@ -41,5 +45,35 @@ public class Hw {
         String strout = (str.lastIndexOf("1a2b")==18) ? "Номер документа заканчивается 1a2b" : "Номер документа не заканчивается 1a2b";
         return strout;
     }
+
+    boolean check (String str){
+        String[] st = str.split ("");
+        String s = "1234567890";
+        String s2 = "-";
+        if (!s.contains(st[0])) return false;
+        if (!s.contains(st[1])) return false;
+        if (!s.contains(st[2])) return false;
+        if (!s.contains(st[3])) return false;
+        if (!s2.contains(st[4])) return false;
+        if (s.contains(st[5])) return false;
+        if (s.contains(st[6])) return false;
+        if (s.contains(st[7])) return false;
+        if (!s2.contains(st[8])) return false;
+        if (!s.contains(st[9])) return false;
+        if (!s.contains(st[10])) return false;
+        if (!s.contains(st[11])) return false;
+        if (!s.contains(st[12])) return false;
+        if (!s2.contains(st[13])) return false;
+        if (s.contains(st[14])) return false;
+        if (s.contains(st[15])) return false;
+        if (s.contains(st[16])) return false;
+        if (!s2.contains(st[17])) return false;
+        if (!s.contains(st[18])) return false;
+        if (s.contains(st[19])) return false;
+        if (!s.contains(st[20])) return false;
+        if (s.contains(st[21])) return false;
+        return true;
+    }
+
 
 }
--- src/com/dsd/Main.java
+++ src/com/dsd/Main.java
@@ -6,18 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("\tHomeWork");
+
         Hw hw = new Hw();
       //  Scanner sc = new Scanner(System.in);
       //  String documentNum = sc.nextLine();
-        String documentNum = "5554-abc-5558-где-1a2b";
+        String documentNum = "5555-abc-5558-где-1a2b";
 
-        System.out.println(hw.first(documentNum));
-        System.out.println(hw.second(documentNum));
-        System.out.println(hw.third(documentNum));
-        System.out.println(hw.fourth(documentNum));
-        System.out.println(hw.fifth(documentNum));
-        System.out.println(hw.sixth(documentNum));
-        System.out.println(hw.seventh(documentNum));
+        if (hw.check(documentNum)) {
+            System.out.println(hw.first(documentNum));
+            System.out.println(hw.second(documentNum));
+            System.out.println(hw.third(documentNum));
+            System.out.println(hw.fourth(documentNum));
+            System.out.println(hw.fifth(documentNum));
+            System.out.println(hw.sixth(documentNum));
+            System.out.println(hw.seventh(documentNum));
+        }
+        else System.out.println("Неккоректный ввод");
+
+        System.out.println("\n\tMade by Dmitry Drako (─‿‿─)");
 
     }
 }
